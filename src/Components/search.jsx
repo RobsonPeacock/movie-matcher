5feb25ff971e745cd15e4ef391c0cbaf562e1bd9
Robson Peacock <robson_peacock20@outlook.com>
2025-02-28 11:37:10 +0000

    Implement real-time search updates on input change

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -4,13 +4,10 @@ import movies from '../movies.json'
 const Search = () => {
   const [searchInput, setSearchInput] = useState('');
   const [result, setResult] = useState('');
+  const titles = movies.map(movie => movie.title);
 
   const handleChange = (event) => {
     setSearchInput(event.target.value);
-  }
-
-  const handleClick = () => {
-    const titles = movies.map(movie => movie.title);
     const newRegex = new RegExp(searchInput, 'i');
     setResult(titles.filter(title => title.match(newRegex)));
   }
@@ -31,10 +28,6 @@ const Search = () => {
 
       <div class="row">
         <output>{result}</output>
-      </div>
-
-      <div class="row">
-        <button class="btn btn-primary" onClick={handleClick}>Search</button>
       </div>
     </>
   )
